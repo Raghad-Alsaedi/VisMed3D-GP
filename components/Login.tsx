85a103e2deaca36faa2151dcba2af181e37fa1ea
Raghad-Alsaedi <160268081+Raghad-Alsaedi@users.noreply.github.com>
2026-01-31 12:33:49 +0300

    Update Login.tsx

--- components/Login.tsx
+++ components/Login.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { z } from "zod";
 import { useState } from "react";
+import Link from "next/link";
 
 const loginSchema = z.object({
   username: z.string().min(1, "Username is required"),
