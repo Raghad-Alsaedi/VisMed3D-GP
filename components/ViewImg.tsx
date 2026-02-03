ef1aefbff36a881d06f22ccab8cdc047648cc8f1
Raghad-Alsaedi <160268081+Raghad-Alsaedi@users.noreply.github.com>
2026-02-03 21:13:15 +0300

    Update ViewImg.tsx

--- components/ViewImg.tsx
+++ components/ViewImg.tsx
@@ -12,7 +12,7 @@ const ViewImg = () => {
 
       <div className="mt-4 flex-1 bg-[#0D1A2D] border border-white/20 rounded-lg flex items-center justify-center relative min-h-0">
         <Img />
-        <h2 className="text-white/30 font-medium bottom-0 right-0 absolute m-2 text-[14px]"> 30 FPS</h2>
+        <h2 className="text-white/30 font-medium bottom-0 left-0 absolute m-2 text-[14px]"> 30 FPS</h2>
       </div>
 
       <div className="flex-shrink-0">
